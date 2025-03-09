18f385175180921c3a2ba94505e446965e7e4b8e
qaffe <riley.connor.cross@gmail.com>
2025-03-09 05:00:34 -0700

    Edited nvim config, added plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- NVIM OPTIONS
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 -- INITIALIZE PLUGINS
 local vim = vim
 local Plug = vim.fn['plug#']
@@ -6,6 +10,7 @@ vim.call('plug#begin')
 
 Plug('echasnovski/mini.nvim') -- mini.nvim
 Plug('echasnovski/mini.hipatterns') -- Highlight Patterns
+Plug('lukas-reineke/indent-blankline.nvim')
 
 vim.call('plug#end')
 
@@ -18,3 +23,4 @@ hipatterns.setup({
 		hex_color = hipatterns.gen_highlighter.hex_color(),
 	},
 })
+require("ibl").setup()
