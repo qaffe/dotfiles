c579c733f21517246780de61d5a95743b14d130e
qaffe <riley.connor.cross@gmail.com>
2025-04-27 10:26:52 -0700

    formatting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,20 +18,20 @@ vim.keymap.set("ca", "ex", "Yazi")	-- easier to access yazi
 -- hardmode
 local hardmode = true
 if hardmode then
-    -- show an error message if a disabled key is pressed
-    local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
+	-- show an error message if a disabled key is pressed
+	local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
 
-    -- disable arrow keys in insert mode with a styled message
-    vim.api.nvim_set_keymap("i", "<Up>", "<C-o>" .. msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap("i", "<Down>", "<C-o>" .. msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap("i", "<Left>", "<C-o>" .. msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap("i", "<Right>", "<C-o>" .. msg, { noremap = true, silent = false })
+	-- disable arrow keys in insert mode with a styled message
+	vim.api.nvim_set_keymap("i", "<Up>", "<C-o>" .. msg, { noremap = true, silent = false })
+	vim.api.nvim_set_keymap("i", "<Down>", "<C-o>" .. msg, { noremap = true, silent = false })
+	vim.api.nvim_set_keymap("i", "<Left>", "<C-o>" .. msg, { noremap = true, silent = false })
+	vim.api.nvim_set_keymap("i", "<Right>", "<C-o>" .. msg, { noremap = true, silent = false })
 
-    -- disable arrow keys in normal mode with a styled message
-    vim.api.nvim_set_keymap("n", "<Up>", msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap("n", "<Down>", msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap("n", "<Left>", msg, { noremap = true, silent = false })
-    vim.api.nvim_set_keymap("n", "<Right>", msg, { noremap = true, silent = false })
+	-- disable arrow keys in normal mode with a styled message
+	vim.api.nvim_set_keymap("n", "<Up>", msg, { noremap = true, silent = false })
+	vim.api.nvim_set_keymap("n", "<Down>", msg, { noremap = true, silent = false })
+	vim.api.nvim_set_keymap("n", "<Left>", msg, { noremap = true, silent = false })
+	vim.api.nvim_set_keymap("n", "<Right>", msg, { noremap = true, silent = false })
 end
 
 vim.api.nvim_create_autocmd("VimEnter", { -- runs after the editor is ready
@@ -76,7 +76,6 @@ vim.call("plug#end")
 --╚═╝     ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝╚═╝  ╚═══╝     ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝     ╚═╝ ╚═════╝ 
 
 local hipatterns = require("mini.hipatterns") 					-- mini highlight patterns
-
 hipatterns.setup({
 	highlighters = {
 		-- add TODO, FIX, etc here later
